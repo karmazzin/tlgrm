da69af4a4e3dca1829a5391e52a74225af555f8c
Karmazzin <karmazzin@ya.ru>
2016-02-13 00:17:28 +0300

    add cat

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var TelegramBotApi = require('node-telegram-bot-api');
-var bot = new TelegramBotApi(process.env.TLGRM_TOKEN, { port: process.env.PORT });
+var bot = new TelegramBotApi(process.env.TLGRM_TOKEN, {polling: true});
+
+bot.onText(/\/cat(.*)/, function (msg, match) {
+    var fromId = msg.from.id;
+    var photo = 'cat.jpg';
+    bot.sendPhoto(fromId, photo, {caption: 'Lovely kittens'});
+});
 
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
-    console.log(msg);
     bot.sendMessage(chatId, msg.text, {caption: "I'm a bot!"});
 });
